7ba5a7b2b0906063b064197d17fd8f80683c6685
Ershov Vadim <Vadim.Ershov@raiffeisen.ru>
2023-10-18 23:00:29 +0300

    init

---
+++ src/main/java/ru/itmo/communal/CommunalServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.communal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunalServiceApplication.class, args);
+	}
+
+}

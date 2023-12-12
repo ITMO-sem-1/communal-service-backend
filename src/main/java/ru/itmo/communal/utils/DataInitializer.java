fa55747182a8f46ad21fe43a41ca93e19edcd1f6
Шишигин Степан <sshishigin190101@gmail.com>
2023-12-13 02:00:36 +0300

    инициализация дефолтных ком услуг в бд

--- src/main/java/ru/itmo/communal/utils/DataInitializer.java
+++ src/main/java/ru/itmo/communal/utils/DataInitializer.java
@@ -2,24 +2,32 @@ package ru.itmo.communal.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
+import ru.itmo.communal.entity.PublicUtility;
 import ru.itmo.communal.entity.SubscriberAddress;
+import ru.itmo.communal.repository.PublicUtilityRepository;
 import ru.itmo.communal.repository.SubscriberAddressRepository;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private final SubscriberAddressRepository subscriberAddressRepository;
+    private final PublicUtilityRepository publicUtilityRepository;
 
     @Autowired
-    public DataInitializer(SubscriberAddressRepository subscriberAddressRepository) {
+    public DataInitializer(SubscriberAddressRepository subscriberAddressRepository, PublicUtilityRepository publicUtilityRepository) {
         this.subscriberAddressRepository = subscriberAddressRepository;
+        this.publicUtilityRepository = publicUtilityRepository;
+
     }
 
     @Override
     public void run(String... args) throws Exception {
+        //Создание адреса
         Optional<SubscriberAddress> address = subscriberAddressRepository.getSubscriberAddressByAccountNumber("123456");
         if (address.isEmpty()) {
             SubscriberAddress subscriberAddress = new SubscriberAddress();
@@ -28,5 +36,19 @@ public class DataInitializer implements CommandLineRunner {
             subscriberAddressRepository.save(subscriberAddress);
         }
 
+        // Создание ком. услуг
+        var defaultPublicUtilities = new HashMap<String, String>();
+        defaultPublicUtilities.put("Холодная вода", "чашки");
+        defaultPublicUtilities.put("Горячая вода", "чашки");
+        defaultPublicUtilities.put("Тепло", "Сожженые свечки");
+        defaultPublicUtilities.put("Электричество", "молнии");
+
+        for (var entry : defaultPublicUtilities.entrySet()) {
+            PublicUtility utility = PublicUtility.builder().name(entry.getKey()).measure(entry.getValue()).build();
+            if (!publicUtilityRepository.exists(Example.of(utility))) {
+                publicUtilityRepository.save(utility);
+            }
+        }
     }
 }
+

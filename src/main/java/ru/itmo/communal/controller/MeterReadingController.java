a5e1a6ba266a781ad4ada49f6fba18ce73178a4c
Шишигин Степан <sshishigin190101@gmail.com>
2024-01-15 19:10:52 +0300

    подача показаний

---
+++ src/main/java/ru/itmo/communal/controller/MeterReadingController.java
@@ -0,0 +1,40 @@
+package ru.itmo.communal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itmo.communal.controller.dto.SubmitMeterReadingRequest;
+import ru.itmo.communal.entity.MeterReading;
+import ru.itmo.communal.repository.MeterReadingRepository;
+import ru.itmo.communal.repository.PublicUtilityRepository;
+import ru.itmo.communal.repository.SubscriberAddressRepository;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/meter-reading")
+public class MeterReadingController {
+    @Autowired
+    MeterReadingRepository meterReadingRepository;
+
+    @Autowired
+    PublicUtilityRepository publicUtilityRepository;
+
+    @Autowired
+    SubscriberAddressRepository subscriberAddressRepository;
+
+    @PostMapping
+    public void submitMeterReadings(@RequestBody SubmitMeterReadingRequest meterReadingRequest) {
+        MeterReading meterReading = MeterReading.builder()
+                .value(meterReadingRequest.getValue())
+                .utility(publicUtilityRepository.findById(meterReadingRequest.getUtilityId()).orElseThrow())
+                .datetime(LocalDateTime.now())
+                .address(subscriberAddressRepository.findById(meterReadingRequest.getAddressId()).orElseThrow())
+                .build();
+        meterReadingRepository.save(
+                meterReading
+        );
+    }
+}

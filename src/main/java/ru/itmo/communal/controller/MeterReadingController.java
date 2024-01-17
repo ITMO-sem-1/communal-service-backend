450087363447b31fd0fb7ea61a9018a76bbaf7c2
makar-pelogeiko <m.pelogeiko@mail.ru>
2024-01-17 21:57:14 +0300

    MeterReadingController

--- src/main/java/ru/itmo/communal/controller/MeterReadingController.java
+++ src/main/java/ru/itmo/communal/controller/MeterReadingController.java
@@ -1,9 +1,12 @@
 package ru.itmo.communal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.itmo.communal.controller.dto.SubmitMeterReadingRequest;
 import ru.itmo.communal.entity.MeterReading;
+import ru.itmo.communal.entity.PublicUtility;
+import ru.itmo.communal.entity.SubscriberAddress;
 import ru.itmo.communal.repository.MeterReadingRepository;
 import ru.itmo.communal.repository.PublicUtilityRepository;
 import ru.itmo.communal.repository.SubscriberAddressRepository;
@@ -34,5 +37,17 @@ public class MeterReadingController {
         meterReadingRepository.save(
                 meterReading
         );
+    }
+
+    @GetMapping("/last/{addrId}/{utilId}")
+    public ResponseEntity<?> getLaseMeterReadings(@PathVariable Integer addrId, @PathVariable Integer utilId) {
+        SubscriberAddress address = subscriberAddressRepository.findById(addrId).orElseThrow(() -> new IllegalStateException("Несуществующий адрес"));
+        PublicUtility utility = publicUtilityRepository.findById(utilId).orElseThrow(() -> new IllegalStateException("Несуществующая услуга"));
+        MeterReading mr = meterReadingRepository.findFirstByAddressAndUtilityOrderByDatetimeDesc(address, utility).orElseThrow(() -> new IllegalStateException("нет ни одного показания"));
+        return ResponseEntity.ok().body(SubmitMeterReadingRequest.builder()
+                .addressId(addrId)
+                .utilityId(utilId)
+                .value(mr.getValue())
+                .build());
     }
 }
--- src/main/java/ru/itmo/communal/repository/MeterReadingRepository.java
+++ src/main/java/ru/itmo/communal/repository/MeterReadingRepository.java
@@ -1,5 +1,6 @@
 package ru.itmo.communal.repository;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.itmo.communal.entity.MeterReading;
@@ -8,9 +9,12 @@ import ru.itmo.communal.entity.SubscriberAddress;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MeterReadingRepository extends JpaRepository<MeterReading, Integer> {
 //    List<MeterReading> findMeterReadingsByAddressAndDatetimeGreaterThanEqualAndDatetimeLessThanEqualOrderByDatetimeDesc(SubscriberAddress address, LocalDateTime from, LocalDateTime to);
     @Query(value = "select m from MeterReading m where m.address = ?1 and m.datetime = (select MAX(m1.datetime) from MeterReading m1 where m.address = m1.address and m1.datetime between ?3 and ?4 and m1.utility=?2)")
     MeterReading getLastMeterReadingForAddress(SubscriberAddress address, PublicUtility utility, LocalDateTime from, LocalDateTime to);
+
+    Optional<MeterReading> findFirstByAddressAndUtilityOrderByDatetimeDesc(SubscriberAddress address, PublicUtility utility);
 }
